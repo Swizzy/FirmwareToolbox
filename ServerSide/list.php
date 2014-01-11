5ca51e6d6b4e6a8e34cc9c8a83e2eda5ca77356f
Swizzy <swizzy@hotmail.co.uk>
2014-01-11 23:07:32 +0100

    Moved list.php towards beeing more dynamic (no more 1 file per directory)
    Signed-off-by: Swizzy <swizzy@hotmail.co.uk>

--- ServerSide/list.php
+++ ServerSide/list.php
@@ -1,21 +1,10 @@
 <?php
-function curPageURL() {
- $pageURL = 'http';
- if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
-
- return substr($pageURL, 0, strrpos($pageURL,"/")+1);
-}
-$site = curPageURL();
+$dir = substr($_SERVER["PATH_INFO"], 1).'/';
+$site = "http://".$_SERVER["SERVER_NAME"].'/download.php/'.$dir;
 $xml = new SimpleXMLElement('<xml/>');
-$ffs = scandir('.');
+$ffs = scandir($dir);
 foreach($ffs as $ff){
- if($ff != '.' && $ff != '..' && !is_dir($ff)) {
+ if($ff != '.' && $ff != '..' && !is_dir($dir.$ff)) {
     $ext = pathinfo($ff, PATHINFO_EXTENSION);
     if (strcasecmp($ext, 'php') == 0)
             continue;
@@ -25,22 +14,24 @@ foreach($ffs as $ff){
             continue;
     if (strcasecmp($ext, 'msg') == 0)
             continue;
+    if (strcasecmp($ext, 'count') == 0)
+            continue;
     $entry = $xml->addChild('entry');
     $file = pathinfo($ff, PATHINFO_FILENAME);
-    if (file_exists($file.'.name'))
-        $entry->addAttribute('name', file_get_contents($file.'.name'));
+    if (file_exists($dir.$file.'.name'))
+        $entry->addAttribute('name', file_get_contents($dir.$file.'.name'));
     else
         $entry->addAttribute('name', $file);
-    if (file_exists($file.'.hash'))
-        $entry->addAttribute('hash', file_get_contents($file.'.hash'));
+    if (file_exists($dir.$file.'.hash'))
+        $entry->addAttribute('hash', file_get_contents($dir.$file.'.hash'));
     else {
-        $hash = md5_file($ff);
+        $hash = md5_file($dir.$ff);
         $entry->addAttribute('hash', $hash);
         file_put_contents($file.'.hash', $hash);
     }            
     $entry->addAttribute('url',$site.$ff);
-    if (file_exists($file.'.msg'))
-        $entry->addAttribute('msg',file_get_contents($file.'.msg'));
+    if (file_exists($dir.$file.'.msg'))
+        $entry->addAttribute('msg',file_get_contents($dir.$file.'.msg'));
     else
         $entry->addAttribute('msg','');
  }
