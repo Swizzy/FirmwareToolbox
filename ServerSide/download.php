eebae282e8fc46b2c300d5fab61be705a92bdf99
Swizzy <swizzy@hotmail.co.uk>
2014-01-11 20:11:11 +0100

    Moved server side scripts to a seperate folder Added download script (count downloads!) Added some scripts to get update information from Sony (going to make 'em download the updates also later...
    Signed-off-by: Swizzy <swizzy@hotmail.co.uk>

---
+++ ServerSide/download.php
@@ -0,0 +1,34 @@
+<?php
+function readfile_chunked ($fname) {
+    $chunksize = 1*(1024*1024); // how many bytes per chunk 
+    $buffer = ''; 
+    $handle = fopen($fname, 'rb'); 
+    if ($handle === false)
+        return false; 
+    while (!feof($handle)) { 
+        $buffer = fread($handle, $chunksize); 
+        print $buffer; 
+    } 
+    return fclose($handle); 
+}
+$file = substr($_SERVER["PATH_INFO"], 1);
+if (file_exists($file)) {
+    //Send file...
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+    header("Content-Disposition: attachment; filename=\"".basename($file)."\";" );
+    header("Content-Transfer-Encoding: binary");
+    header("Content-Length: ".filesize($file));
+    readfile_chunked($file);
+    //Update count...
+    $cntfile = substr($file, 0, strrpos($file, ".")).".count";
+    $downCount = 1;
+    if (file_exists($cntfile))
+        $downCount += intval(file_get_contents($cntfile));        
+    file_put_contents($file ,$downCount);
+}
+?>
---
+++ ServerSide/ps3.php
@@ -0,0 +1,39 @@
+<?php
+$data = file_get_contents('http://fus01.ps3.update.playstation.net/update/ps3/list/us/ps3-updatelist.txt');
+//echo $data;
+foreach (explode(PHP_EOL, $data) as $line) {
+    $ispatch = 0;
+    $ver = "";
+    $url = "";
+    $hash = "";
+    foreach (explode(";", $line) as $part) {
+        if (strlen($ver) > 0 && strlen($url) > 0) {
+            if ($ispatch > 0) {
+                $patchver = $ver;
+                $patchurl = $url;
+                $patchhash = $hash;
+            }
+            else {
+                $sysver = $ver;
+                $sysurl = $url;
+                $syshash = $hash;
+            }
+        }
+        if (stripos($part, "SystemSoftwareVersion=") === 0)
+            $ver = substr($part, strpos($part, "=") + 1);
+        if (stripos($part, "CDN=") === 0) {
+            $url = substr($part, strpos($part, "=") + 1);
+            $hash = substr($url, strrpos($url, '_') + 1, 32);
+            $ispatch = stripos($part, "PS3PATCH.PUP");
+        }
+    }
+}
+echo "patch:\n";
+echo $patchver."\n";
+echo $patchurl."\n";
+echo $patchhash."\n";
+echo "system:\n";
+echo $sysver."\n";
+echo $sysurl."\n";
+echo $syshash;
+?>
